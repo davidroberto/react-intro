6d16ec5deda57776e5f9002a428a8d2ce9d2ef82
David Robert <contact@david-robert.fr>
2025-12-03 11:00:35 +0100

    use state for rewards

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -3,15 +3,41 @@ import {useState} from "react";
 const CookieClicker = () => {
 
    const [cookieCount, setCookieCount] = useState(0);
+   const [rewards, setRewards] = useState([]);
 
     const handleCookieClick = () => {
         setCookieCount(cookieCount + 1);
+
+        if (cookieCount === 9) {
+            setRewards([...rewards, "sandwich à la fraise"]);
+        }
+
+        if (cookieCount === 19) {
+            setRewards([...rewards, "abris bus"]);
+
+        }
+
+        if (cookieCount === 29) {
+            setRewards([...rewards, "Bill du bigdeal"]);
+        }
     };
+
+
 
     return (
         <>
             <button onClick={handleCookieClick}>🍪</button>
             <span>{cookieCount}</span>
+
+            <aside>
+                <h2>Récompenses :</h2>
+                <ul>
+                    {rewards.map((reward, index) => (
+                        <li key={index}>{reward}</li>
+                    ))}
+                </ul>
+            </aside>
+
         </>
     )
 }
