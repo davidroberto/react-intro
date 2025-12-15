aa4739bb0475bcb7bb250220d8ce078a13f517fe
David Robert <contact@david-robert.fr>
2025-12-15 13:26:14 +0100

    simulate use effect for fetching products

--- src/component/ListArticles.jsx
+++ src/component/ListArticles.jsx
@@ -5,6 +5,13 @@ const ListArticles = () => {
   const [products, setProducts] = useState([]);
 
 
+    // au premier chargement du composant
+    // le state products contient un tableau vide
+    // donc on passe dans le if
+    // donc : on fait une requete fetch pour aller chercher les produits
+    // et on les stocke dans le state products via setProducts
+    // ce qui recharge le composant (au second chargement
+    // on ne passe pas dans le if car le tableau n'est plus vide)
   if (products.length === 0) {
     fetch('https://fakestoreapi.com/products')
         .then(response => response.json())
@@ -17,6 +24,9 @@ const ListArticles = () => {
       <h1>Lists Products</h1>
 
       <section>
+        <div>
+            {products.length === 0 && <p>Loading products...</p>}
+        </div>
         {products.map(product => (
             <article key={product.id}>
                 <h2>{product.title}</h2>
