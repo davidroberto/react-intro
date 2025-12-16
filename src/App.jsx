b2319cdacee5f4db17db54d3f50a75d2299acda8
David Robert <contact@david-robert.fr>
2025-12-16 08:33:17 +0100

    show product by id

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import ListArticles from "./component/ListArticles.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import ShowArticlePage from "./pages/ShowArticlePage.jsx";
 
 function App() {
   return (
-    <>
-        <ListArticles />
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<ListArticles />} />
+            <Route path="/articles/:id" element={<ShowArticlePage />} />
+        </Routes>
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ShowArticlePage.jsx
@@ -0,0 +1,33 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+const ShowArticlePage = () => {
+
+    // Récupère le paramètre id depuis l'URL
+
+    const {id} = useParams();
+
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        // récupère le produit sur l'api en utilisant l'id
+        // de l'url
+        fetch("https://fakestoreapi.com/products/" + id)
+            .then(response => response.json())
+            .then(data => setProduct(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>Show product {id}</h1>
+
+            {product && (<div>
+                <h2>{product.title}</h2>
+                <p>Prix : {product.price}</p>
+            </div>
+            )}
+        </div>
+    )
+}
+
+export default ShowArticlePage;
